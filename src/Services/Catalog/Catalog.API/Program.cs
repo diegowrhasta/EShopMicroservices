fda1762fbdf860401edc397dc75399a4801a1e89
Diego Balderrama <diegowrhasta@gmail.com>
2024-04-06 09:23:24 -0400

    Finishing baseline for building blocks, carter module for Catalog.API and tested with roundtrip

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCarter(configurator: c =>
+{
+    var modules = typeof(Program)
+        .Assembly.GetTypes()
+        .Where(t => t.IsAssignableTo(typeof(ICarterModule)))
+        .ToArray();
+    c.WithModules(modules);
+});
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.MapCarter();
 
 app.Run();
