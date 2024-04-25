50de455cdf7afd6b3597117c7fc950b653e9ff02
Diego Balderrama <diegowrhasta@gmail.com>
2024-04-25 08:15:43 -0400

    Implementing GetBasketEndpoints, Global Usings and StoreBasket feature (plus Carter module configuration in DI)

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoints.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoints.cs
@@ -1,5 +1,28 @@
 ﻿namespace Basket.API.Basket.GetBasket;
 
-public class GetBasketEndpoints
+// public record GetBasketRequest(string userName);
+public record GetBasketResponse(ShoppingCart Cart);
+
+public class GetBasketEndpoints : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet(
+                "/basket/{userName}",
+                async (string userName, ISender sender) =>
+                {
+                    var result = await sender.Send(new GetBasketQuery(userName));
+
+                    var response = result.Adapt<GetBasketResponse>();
+
+                    return Results.Ok(response);
+                }
+            )
+            .WithName("GetBasketByUserName")
+            .Produces<GetBasketResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Basket by User Name")
+            .WithDescription("GetBasketByUserName");
+        ;
+    }
 }
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,6 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var assembly = typeof(Program).Assembly;
+
+builder.Services.AddCarter(configurator: c =>
+{
+    var modules = assembly.GetTypes().Where(t => t.IsAssignableTo(typeof(ICarterModule))).ToArray();
+    c.WithModules(modules);
+});
 
 var app = builder.Build();
 
