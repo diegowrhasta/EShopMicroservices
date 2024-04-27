8ebde71080789a195b46f737bd8a9a126d5f4c9a
Diego Balderrama <diegowrhasta@gmail.com>
2024-04-27 08:13:00 -0400

    Setting up repository plus exceptions and configurations to connect to database (Basket)

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -3,7 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var assembly = typeof(Program).Assembly;
 
-builder.Services.AddMediatR(config => 
+builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(assembly);
     config.AddOpenBehavior(typeof(ValidationBehavior<,>));
@@ -14,6 +14,13 @@ builder.Services.AddCarter(configurator: c =>
     var modules = assembly.GetTypes().Where(t => t.IsAssignableTo(typeof(ICarterModule))).ToArray();
     c.WithModules(modules);
 });
+
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+    // opts.AutoCreateSchemaObjects
+}).UseLightweightSessions();
 
 var app = builder.Build();
 
