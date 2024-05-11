abddac82b9776bc3a32cd7ca9b18e2be3d0f35d4
Diego Balderrama <diegowrhasta@gmail.com>
2024-05-11 07:58:45 -0400

    Adding git ignore for auxiliary sqlite files, and implementing GetDiscount request for gRPC service

--- src/Services/Discount/Discount.gRPC/GlobalUsings.cs
+++ src/Services/Discount/Discount.gRPC/GlobalUsings.cs
@@ -3,3 +3,5 @@ global using Grpc.Core;
 global using Discount.gRPC.Services;
 global using Discount.gRPC.Data;
 global using Microsoft.EntityFrameworkCore;
+global using Discount.gRPC.Models;
+global using Mapster;
--- src/Services/Discount/Discount.gRPC/Services/DiscountService.cs
+++ src/Services/Discount/Discount.gRPC/Services/DiscountService.cs
@@ -1,24 +1,56 @@
 ﻿namespace Discount.gRPC.Services;
 
-public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+public class DiscountService(DiscountContext dbContext, ILogger<DiscountService> logger)
+    : DiscountProtoService.DiscountProtoServiceBase
 {
-    public override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+    public override async Task<CouponModel> GetDiscount(
+        GetDiscountRequest request,
+        ServerCallContext context
+    )
     {
-        // TODO: GetDiscount from Database
-        return base.GetDiscount(request, context);
+        var coupon = await dbContext.Coupons.FirstOrDefaultAsync(x =>
+            x.ProductName == request.ProductName
+        );
+
+        if (coupon is null)
+        {
+            coupon = new Coupon
+            {
+                ProductName = "No Discount",
+                Amount = 0,
+                Description = "No Discount Desc"
+            };
+        }
+
+        logger.LogInformation(
+            "Discount is retrieved for ProductName: {productName}, Amount: {amount}",
+            coupon.ProductName,
+            coupon.Amount
+        );
+
+        return coupon.Adapt<CouponModel>();
     }
 
-    public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+    public override Task<CouponModel> CreateDiscount(
+        CreateDiscountRequest request,
+        ServerCallContext context
+    )
     {
         return base.CreateDiscount(request, context);
     }
 
-    public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+    public override Task<CouponModel> UpdateDiscount(
+        UpdateDiscountRequest request,
+        ServerCallContext context
+    )
     {
         return base.UpdateDiscount(request, context);
     }
 
-    public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+    public override Task<DeleteDiscountResponse> DeleteDiscount(
+        DeleteDiscountRequest request,
+        ServerCallContext context
+    )
     {
         return base.DeleteDiscount(request, context);
     }
