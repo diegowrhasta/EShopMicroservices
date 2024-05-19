e07eb3cbebe2f5428dfed7d5e4b00f03f8060e82
Diego Balderrama <diegowrhasta@gmail.com>
2024-05-19 09:37:30 -0400

    All Layers for Ordering MS plus DI base configurations

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -2,6 +2,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services
+     .AddApplicationServices()
+     .AddInfrastructureServices(builder.Configuration)
+     .AddApiServices();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
