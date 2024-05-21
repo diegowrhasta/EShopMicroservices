927953a45d963eabe7832adbef18af4e43faa61c
Diego Balderrama <diegowrhasta@gmail.com>
2024-05-21 08:32:22 -0400

    Developing all domain modeling for Ordering.Domain

---
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿namespace Ordering.Domain.Models;
+
+public class Order : Aggregate<Guid>
+{
+    private readonly List<OrderItem> _orderItems = [];
+    public IReadOnlyList<OrderItem> OrderItems => _orderItems.AsReadOnly();
+
+    public Guid CustomerId { get; set; } = default!;
+    public string OrderName { get; set; } = default!;
+    public Address ShippingAddress { get; set; } = default!;
+    public Address BillingAddress { get; set; } = default!;
+    public Payment Payment { get; set; } = default!;
+    public OrderStatus Status { get; set; } = OrderStatus.Pending;
+    public decimal TotalPrice
+    {
+        get => OrderItems.Sum(x => x.Price * x.Quantity);
+        private set { }
+    }
+}
