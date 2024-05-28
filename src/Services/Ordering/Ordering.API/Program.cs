54f40b939844f1ba7904fc9656168a2b2a9696fb
Diego Balderrama <diegowrhasta@gmail.com>
2024-05-28 08:50:24 -0400

    Auto migrate for Ordering.API

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -10,5 +10,11 @@ builder.Services
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseApiServices();
+
+if (app.Environment.IsDevelopment())
+{
+    await app.InitializeDatabaseAsync();
+}
 
 app.Run();
