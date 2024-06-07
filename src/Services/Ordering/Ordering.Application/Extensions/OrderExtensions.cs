582d6102d4a77740b9b32382dd1e50baaf86920e
Diego Balderrama <diegowrhasta@gmail.com>
2024-06-07 08:53:32 -0400

    Abstracting Order entity to dto logic in an extension method

---
+++ src/Services/Ordering/Ordering.Application/Extensions/OrderExtensions.cs
@@ -0,0 +1,49 @@
+﻿namespace Ordering.Application.Extensions;
+
+public static class OrderExtensions
+{
+    public static IEnumerable<OrderDto> ToOrderDtoList(
+        this IEnumerable<Order> orders
+    )
+    {
+        return orders.Select(o => new OrderDto(
+            o.Id.Value,
+            o.CustomerId.Value,
+            o.OrderName.Value,
+            new AddressDto(
+                o.ShippingAddress.FirstName,
+                o.ShippingAddress.LastName,
+                o.ShippingAddress.EmailAddress!,
+                o.ShippingAddress.AddressLine,
+                o.ShippingAddress.Country,
+                o.ShippingAddress.State,
+                o.ShippingAddress.ZipCode
+            ),
+            new AddressDto(
+                o.BillingAddress.FirstName,
+                o.BillingAddress.LastName,
+                o.BillingAddress.EmailAddress!,
+                o.BillingAddress.AddressLine,
+                o.BillingAddress.Country,
+                o.BillingAddress.State,
+                o.BillingAddress.ZipCode
+            ),
+            new PaymentDto(
+                o.Payment.CardName!,
+                o.Payment.CardNumber,
+                o.Payment.Expiration,
+                o.Payment.CVV,
+                o.Payment.PaymentMethod
+            ),
+            o.Status,
+            o.OrderItems
+                .Select(oi => new OrderItemDto(
+                    oi.OrderId.Value,
+                    oi.ProductId.Value,
+                    oi.Quantity,
+                    oi.Price
+                ))
+                .ToList()
+        ));
+    }
+}
