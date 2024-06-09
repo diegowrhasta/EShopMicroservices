8dc5359a61ae35f32c4bd885d3cd0a5e55baa9a6
Diego Balderrama <diegowrhasta@gmail.com>
2024-06-09 08:58:58 -0400

    Creation of endpoints on Ordering.API (CreateOrder use case)

--- src/Services/Ordering/Ordering.API/GlobalUsings.cs
+++ src/Services/Ordering/Ordering.API/GlobalUsings.cs
@@ -1,5 +1,8 @@
-﻿global using Ordering.API;
+﻿global using Carter;
+global using Mapster;
+global using MediatR;
+global using Ordering.API;
 global using Ordering.Application;
+global using Ordering.Application.Dtos;
 global using Ordering.Infrastructure;
 global using Ordering.Infrastructure.Data.Extensions;
-global using Carter;
