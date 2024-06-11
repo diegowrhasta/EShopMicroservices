e8bb9d205c7caf7b34f1fc97dab788a1cbe9191d
Diego Balderrama <diegowrhasta@gmail.com>
2024-06-11 08:58:34 -0400

    GetOrders endpoint

--- src/Services/Ordering/Ordering.API/Endpoints/GetOrders.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrders.cs
@@ -1,6 +1,33 @@
-﻿namespace Ordering.API.Endpoints
+﻿using BuildingBlocks.Pagination;
+using Ordering.Application.Orders.Queries.GetOrders;
+
+namespace Ordering.API.Endpoints;
+
+// public record GetOrdersRequest(PaginationRequest PaginationRequest);
+public record GetOrdersResponse(PaginatedResult<OrderDto> Orders);
+
+public class GetOrders : ICarterModule
 {
-    public class GetOrders
+    public void AddRoutes(IEndpointRouteBuilder app)
     {
+        app.MapGet(
+                "/orders",
+                async (
+                    [AsParameters] PaginationRequest request,
+                    ISender sender
+                ) =>
+                {
+                    var result = await sender.Send(new GetOrdersQuery(request));
+
+                    var response = result.Adapt<GetOrdersResponse>();
+
+                    return Results.Ok(response);
+                }
+            )
+            .WithName("GetOrders")
+            .Produces<CreateOrderResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("GetOrders")
+            .WithDescription("GetOrders");
     }
 }
