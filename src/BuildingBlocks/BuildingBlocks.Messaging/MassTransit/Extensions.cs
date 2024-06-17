26e4eab615049cc509b104ed9df370eaa40ec0b9
Diego Balderrama <diegowrhasta@gmail.com>
2024-06-17 10:46:39 -0400

    New library for MassTransit with RabbitMQ integration plus configurations on Ordering and Basket

---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BuildingBlocks.Messaging.MassTransit;
+
+public static class Extensions
+{
+    public static IServiceCollection AddMessageBroker(
+        this IServiceCollection services,
+        Assembly? assembly = null
+    )
+    {
+
+        return services;
+    }
+}
