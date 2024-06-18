3c0ff7b3958b780a883ad0e261595fd26ba58d56
Diego Balderrama <diegowrhasta@gmail.com>
2024-06-18 08:24:21 -0400

    Finishing extension method for masstransit in bb

--- src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extensions.cs
+++ src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extensions.cs
@@ -1,4 +1,6 @@
 ﻿using System.Reflection;
+using MassTransit;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BuildingBlocks.Messaging.MassTransit;
@@ -7,9 +9,38 @@ public static class Extensions
 {
     public static IServiceCollection AddMessageBroker(
         this IServiceCollection services,
+        IConfiguration configuration,
         Assembly? assembly = null
     )
     {
+        services.AddMassTransit(config =>
+        {
+            config.SetKebabCaseEndpointNameFormatter();
+
+            if (assembly is not null)
+            {
+                config.AddConsumers(assembly);
+            }
+
+            config.UsingRabbitMq(
+                (context, configurator) =>
+                {
+                    configurator.Host(
+                        new Uri(configuration["MessageBroker:Host"]!),
+                        host =>
+                        {
+                            host.Username(
+                                configuration["MessageBroker:UserName"]!
+                            );
+                            host.Password(
+                                configuration["MessageBroker:Password"]!
+                            );
+                        }
+                    );
+                    configurator.ConfigureEndpoints(context);
+                }
+            );
+        });
 
         return services;
     }
