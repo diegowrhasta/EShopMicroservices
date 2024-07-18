184e21e65b1075801777c9e760c3b30ea899cd18
Diego Balderrama <diegowrhasta@gmail.com>
2024-07-18 10:16:30 -0400

    Adding all Models for Frontend application

---
+++ src/WebApps/Shopping.Web/Models/Ordering/PaginatedResult.cs
@@ -0,0 +1,15 @@
+﻿namespace Shopping.Web.Models.Ordering;
+
+public class PaginatedResult<TEntity>(
+    int pageIndex,
+    int pageSize,
+    long totalCount,
+    IEnumerable<TEntity> data
+)
+    where TEntity : class
+{
+    public int PageIndex { get; } = pageIndex;
+    public int PageSize { get; } = pageSize;
+    public long TotalCount { get; } = totalCount;
+    public IEnumerable<TEntity> Data { get; } = data;
+}
